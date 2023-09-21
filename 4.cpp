2ef0ac0d658384c4664ae06489fe1bd974f53955
perplexity <135210710+voldymoldyy@users.noreply.github.com>
2023-09-21 12:04:17 +0530

    practice

--- 4.cpp
+++ 4.cpp
@@ -10,7 +10,7 @@ int main()
     std::cout << "enter n: ";
     std::cin >> n;
 
-    int arr[m][n];
+    int arr[m][n] = {0};
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
@@ -18,4 +18,21 @@ int main()
             std::cin >> arr[i][j];
         }
     }
+
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum = sum + arr[i][i];
+    }
+
+    int j = n - 1;
+    for (int i = 0; i < m; i++)
+    {
+        sum = sum + arr[j][i];
+        std::cout << arr[j][i];
+        std::cout << "\n";
+        j--;
+    }
+    sum = sum - arr[1][1];
+    std::cout << sum;
 }
